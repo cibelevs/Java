9a7fcc8acf502a2942dc93d8788f6cfb13600428
Cibele Vale Sacramento <cibelevale.estudante@gmail.com>
2025-04-27 20:58:02 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -10,14 +10,16 @@
  */
 public class Curso
 {
+    private String nome;
     private int codigo;
     private int duracaoCurso;
     private float valorTotal;
     private int anoCriacao;
     
     //Criação de um curso com todos os dados necessários;
-    public Curso(int codigo, int duracaoCurso, float valorTotal, int anoCriacao)
+    public Curso(String nome, int codigo, int duracaoCurso, float valorTotal, int anoCriacao)
     {
+        this.nome = nome;
         this.codigo = codigo;
         this.duracaoCurso = duracaoCurso;
         this.valorTotal = valorTotal;
@@ -25,8 +27,8 @@ public class Curso
     }
     
     //Criação de um curso sem informar a duração. Neste caso o default são 8 semestres;
-    public Curso(int codigo,float valorTotal, int anoCriacao){
-         this(codigo, 8, valorTotal, anoCriacao);
+    public Curso(String nome, int codigo,float valorTotal, int anoCriacao){
+         this(nome, codigo, 8, valorTotal, anoCriacao);
     }
 
     //Metódo para calcular o valor da mensalidade de um aluno, considerando que o valor
@@ -35,5 +37,17 @@ public class Curso
         int meses = this.duracaoCurso * 6;
         return this.valorTotal / meses;
     }
+
+    public void setNome(String nome){this.nome = nome;}
+    public void setCodigo(int codigo){this.codigo = codigo;}
+    public void setDuracao(int duracao){this.duracao = duracao;}
+    public void setAnoCriacao(int anoCriacao){this.anoCriacao = anoCriacao;}
+    public void setValorTotal(float valorTotal){this.valorTotal=valorTotal;}
+    
+    public String getNome(){return this.nome;}
+    public int getCodigo(){return this.codigo;}
+    public int getDuracao(){return this.duracao;}
+    public int getAnoCriacao(){return this.anoCriacao;}
+    public float getValorTotal(){return this.valorTotal;}
     
 }
